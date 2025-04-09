1d3ab79f52ba0ec3ee3ab774f6833a9907b1fb00
Alreisyah <alreisyah14@upi.edu>
2025-04-09 21:21:16 +0700

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,22 @@
+var swiper = new Swiper(".swiper", {
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    }
+});
